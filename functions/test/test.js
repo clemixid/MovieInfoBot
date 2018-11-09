f593dfa67731d1e6c39a6dc7623cdcd2aaecfdde
clemixid <clemixid@gmail.com>
2018-11-09 14:10:52 +0100

    using nock for http request

--- functions/test/test.js
+++ functions/test/test.js
@@ -1,7 +1,12 @@
 const assert = require('assert');
 const OMDbApiCaller = require('../index');
+const nock = require('nock');
 
 describe('OMDb Api Call known', () => {
+  beforeEach(() => {
+    const batmanBody = { Title: 'Batman', Year: '1989' };
+    nock('http://www.omdbapi.com').get(() => true).reply(200, batmanBody);
+  });
   it('give info on a movie known', (done) => {
     const batmanMock = { body: { queryResult: { parameters: { 'movie-title': 'batman' } } } };
     const mockOut = { res: '', json(o) { assert(o.fulfillmentText.slice(0, 12) === 'Batman(1989)'); done(); } };
@@ -10,6 +15,10 @@ describe('OMDb Api Call known', () => {
 });
 
 describe('OMDb Api Call unknown', () => {
+  beforeEach(() => {
+    const unknownBody = { Title: undefined };
+    nock('http://www.omdbapi.com').get(() => true).reply(200, unknownBody);
+  });
   it('give info on a movie unknown', (done) => {
     const unknownMock = { body: { queryResult: { parameters: { 'movie-title': '' } } } };
     const mockOut = { res: '', json(o) { assert(o.fulfillmentText.slice(0, 5) === 'Sorry'); done(); } };
