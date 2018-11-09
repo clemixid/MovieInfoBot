499b2e7e18417d7612b0a0d2f91ca8ab53dabe4a
clemixid <clemixid@gmail.com>
2018-11-09 14:47:58 +0100

    improve test

--- functions/test/test.js
+++ functions/test/test.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
-const OMDbApiCaller = require('../index');
 const nock = require('nock');
+const OMDbApiCaller = require('../index');
 
 describe('OMDb Api Call known', () => {
   beforeEach(() => {
